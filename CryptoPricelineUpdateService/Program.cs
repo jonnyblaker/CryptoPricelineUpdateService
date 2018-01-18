e478f67834641cd9cf77b3dec846d99b3c030332
Jonny Blaker <jblack1776@hotmail.com>
2018-01-17 22:10:41 -0500

    jlr\ \

--- CryptoPricelineUpdateService/Program.cs
+++ CryptoPricelineUpdateService/Program.cs
@@ -25,23 +25,60 @@ namespace CryptoPricelineUpdateService
         {
             try
             {
-                string url = @"https://rest.coinapi.io/v1/quotes/current";
+                string url = @"https://rest.coinapi.io/v1/trades/latest";
                 var client = new WebClient();
                 client.Headers.Add("X-CoinAPI-Key", "BADF8493-2F65-40BC-AB81-5B49516243AC");
                 var response = client.DownloadString(url);
-                string s = HttpUtility.HtmlDecode(response.ToString());
-                string x = s;
+                //string s1 = HttpUtility.HtmlDecode(response.ToString());
+                List<RootObject> rbs = JsonConvert.DeserializeObject<List<RootObject>>(response);
 
-                Rootobject rb = JsonConvert.DeserializeObject<Rootobject>(response);
-                foreach (Priceline p in rb.Pricelines)
-                {
+                foreach (RootObject p in rbs)
+                {                    
                     try
                     {
+                        // instance my class to convert data from JSON return class
+                        PricelineRecord r;
+
+                        // break down the string symbol in the four buckets
+                        string[] symbolVals = p.symbol_id.Split('_');
+
+                        // assign MARKET, SYMBOL, CURRENCY vals
+                        r = new PricelineRecord(symbolVals[0].ToString(), symbolVals[2].ToString(), symbolVals[3].ToString());
+                        
+
+                        // Price & Timestamp - Handle conversion to KRW only now
+                        if (r.currency == "KRW")
+                        {
+                            float d = Convert.ToInt32(p.price);
+                            float xratePrice = (d * Convert.ToInt32(.00093));
+                            r.price = xratePrice;
+                            r.timeStamp = p.time_exchange;
+                        }
+                        else
+                        {
+                            r.price = p.price;
+                            r.timeStamp = p.time_exchange;
+                        }
+
+
+                        // call db insert stored procedure
+                        SqlConnection conn = new SqlConnection(@"Data Source=119.81.9.244,780;Initial Catalog=JBlack1776_CCN;Persist Security Info=True;User ID=JBlack1776_sysdba;Password=masterkey");
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand("ADD_PRICELINE", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("MARKET", SqlDbType.NVarChar).Value = r.market;
+                        cmd.Parameters.AddWithValue("SYMBOL", SqlDbType.NVarChar).Value = r.symbol;
+                        cmd.Parameters.AddWithValue("CURRENCY", SqlDbType.NVarChar).Value = r.currency;
+                        cmd.Parameters.AddWithValue("PRICE", SqlDbType.Float).Value = r.price;
+                        cmd.Parameters.AddWithValue("TIMESTAMP", SqlDbType.DateTime).Value = r.timeStamp;
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        conn.Dispose();
 
                     }
                     catch (Exception ex)
                     {
-
+                        //throw ex;
                     }
                 }
 
@@ -49,39 +86,40 @@ namespace CryptoPricelineUpdateService
             }
             catch (Exception ex)
             {
-                throw ex;
+                //throw ex;
             }
         }
-        
-        // JSON Return Classes
-        public class Rootobject
+
+        // my class
+        public class PricelineRecord
         {
-            public Priceline[] Pricelines { get; set; }
+            public string symbol { get; set; }
+            public string currency { get; set; }
+            public string market { get; set; }
+            public double price { get; set; }
+            public double arbitrage { get; set; }
+            public DateTime timeStamp { get; set; }
+
+            public PricelineRecord(string a, string b, string c)
+            {
+                market = a;
+                symbol = b;
+                currency = c;
+            }
         }
 
-        public class Priceline
+        // JSON Return Classes
+
+        public class RootObject
         {
             public string symbol_id { get; set; }
             public DateTime time_exchange { get; set; }
             public DateTime time_coinapi { get; set; }
-            public float ask_price { get; set; }
-            public float ask_size { get; set; }
-            public float bid_price { get; set; }
-            public float bid_size { get; set; }
-            public Last_Trade last_trade { get; set; }
-        }
-
-        public class Last_Trade
-        {
-            public DateTime time_exchange { get; set; }
-            public DateTime time_coinapi { get; set; }
             public string uuid { get; set; }
-            public float price { get; set; }
-            public float size { get; set; }
+            public double price { get; set; }
+            public double size { get; set; }
             public string taker_side { get; set; }
         }
-
-
 
     }
 }
